83fee315d52b223348aa3df0da99e0375da8cc47
Flávia Correia <48125785+fllaviacorreia@users.noreply.github.com>
2023-03-20 19:19:06 -0300

    update app js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ export default function App() {
   return (
     <div className="App">
       <div className="column">{SetMatriz(Matriz42x42)}</div>
+      <div className="column">{SetMatriz(Matriz1_28x28)}</div>
+      <div className="column">{SetMatriz(Matriz2_28x28)}</div>
     </div>
   );
 }
